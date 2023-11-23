c4bf40ecaca36d068aa83e499ae2ba44d28fcd75
Constantin Lerner <colerner@me.com>
2023-11-23 14:05:22 +0100

    list route, controller & view done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "json"
+require "open-uri"
+
+puts 'Cleaning database....'
+Movie.destroy_all
+puts 'Creating movies...'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movies_string = URI.open(url).read
+movies = JSON.parse(movies_string)
+
+movies['results'].each do |movie|
+  movie = Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: movie['poster_path'],
+    rating: movie['vote_average']
+  )
+  puts "Created #{movie.title}"
+end
+
+puts "Finished!"
